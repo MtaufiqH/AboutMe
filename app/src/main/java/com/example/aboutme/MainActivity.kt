c280579ecb0b40723d588a369d9232b0678b70db
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-07-30 10:57:36 +0800

    use data binding for replace findviewbyid

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,64 +1,79 @@
 package com.example.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var nameInput: EditText
-    lateinit var nickname: TextView
-    lateinit var doneButton: Button
+    // variable with ActivityMainBinding type
+    // it's automaticly create by Android Studio
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        nameInput = findViewById(R.id.nickname_edit)
-        nickname = findViewById(R.id.nickname_id)
-        doneButton = findViewById(R.id.button_done)
+        // use dataBindingUtil to setup databinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        doneButton.setOnClickListener {
-            addNickname(it)
+        // replace setContentView
+        // setContentView(R.layout.activity_main)
+
+
+        /* Replace findviewbyid with Binding
+         * nameInput = findViewById(R.id.nickname_edit)
+         * nickname = findViewById(R.id.nickname_id)
+         * doneButton = findViewById(R.id.button_done)
+         */
+
+
+
+        with(binding) {
+            buttonDone.setOnClickListener {
+                addNickname(it)
+            }
+
+            nicknameId.setOnClickListener {
+                updateNickname(it)
+
+            }
         }
-
-        nickname.setOnClickListener {
-            updateNickname(it)
-        }
-
-
 
     }
 
 
     // will show nickname in the UI
     private fun addNickname(view: View) {
-        nickname.text = nameInput.text
-        nickname.visibility = View.VISIBLE
-        nameInput.visibility = View.GONE
-        view.visibility = View.GONE
+        with(binding) {
+            nicknameId.text = nicknameEdit.text.toString()
+            nicknameId.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+        }
+
 
     }
 
     // will activate editText and update the nickname
     private fun updateNickname(view: View) {
-        nameInput.visibility= View.VISIBLE
-        doneButton.visibility = View.VISIBLE
-        view.visibility = View.GONE
+        binding.apply {
+            nicknameEdit.visibility = View.VISIBLE
+            buttonDone.visibility = View.VISIBLE
+            view.visibility = View.GONE
 
-        // set the focus of the edit text
-        nameInput.requestFocus()
+            // set the focus of the edit text
+            nicknameEdit.requestFocus()
 
-        // show keyboard
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(nameInput,0)
+            // show keyboard
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(nicknameEdit, 0)
+
+        }
 
     }
 }
